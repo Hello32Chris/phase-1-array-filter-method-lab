633118f9867c8e1dc61b550161db6674bd41c101
Christopher Orth <christopherorth430@yahoo.com>
2023-08-25 11:59:59 -0400

    array filter lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching(array, string) {
+    return array.filter((name) => name.toLowerCase() === string.toLowerCase());
+}
+
+
+function fuzzyMatch(source, testString) {
+  return source.filter(
+    (possibleMatch) =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+  );
+}
+
+function matchName(array, driverName) {
+  return array.filter(function (prop) {
+    return prop.name === driverName;
+  });
+}
+
